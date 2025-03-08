d1314110e9b96d871f1148c69264e180354527dc
Mohammed Gebriel <smssms15311@gmail.com>
2025-03-08 23:24:28 +0200

    create home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Layout from "./components/organisms/Layout"
+import Home from "./components/pages/home/Home"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Layout />}>
-          <Route index element={<h1>Home</h1>} />
+          <Route index element={<Home />} />
         </Route>
       </Routes>
     </BrowserRouter>
