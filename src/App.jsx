82724955f8d768db03fd40e10daccc3f38df5454
Mohammed Gebriel <smssms15311@gmail.com>
2025-03-08 11:55:44 +0200

    add dark mode file

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import DarkToggle from "./components/dark-mode/DarkToggle"
 
 function App() {
   return (
     <div className="">
+      <DarkToggle />
       <Routes>
         <Route path='/' element={<h1>Home Page</h1>}/>
       </Routes>
